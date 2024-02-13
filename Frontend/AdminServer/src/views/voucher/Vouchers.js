3c12e182001151ec49941cfa15b763a3a2b8d61d
Dương Đức Khải <greenjohnny135@gmail.com>
2024-02-13 14:26:50 +0700

    create a voucher page and add it to routes and nav

---
+++ Frontend/AdminServer/src/views/voucher/Vouchers.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCardHeader,
+  CCol,
+  CLink,
+  CRow,
+  CWidgetStatsB,
+  CWidgetStatsC,
+  CWidgetStatsE,
+  CWidgetStatsF,
+} from '@coreui/react'
+import { getStyle } from '@coreui/utils'
+import CIcon from '@coreui/icons-react'
+import {
+  cilArrowRight,
+  cilBasket,
+  cilBell,
+  cilChartPie,
+  cilMoon,
+  cilLaptop,
+  cilPeople,
+  cilSettings,
+  cilSpeech,
+  cilSpeedometer,
+  cilUser,
+  cilUserFollow,
+} from '@coreui/icons'
+import { CChartBar, CChartLine } from '@coreui/react-chartjs'
+import { DocsExample } from 'src/components'
+
+const Vouchers = () => {
+  const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+  return (
+    <CCard className="mb-4">
+      <CCardHeader>
+        <CRow className="d-flex flex-row align-items-center">
+          <CCol xs={6} sm={6} lg={6} className="justify-content-center">
+            <h3 className="mb-0">Vouchers Management</h3>
+          </CCol>
+          <CCol xs={6} sm={6} lg={6} className="text-end">
+            <CButton color="info">
+              <h4 className="mb-0">Create a new voucher</h4>
+            </CButton>
+          </CCol>
+        </CRow>
+      </CCardHeader>
+      <CCardBody></CCardBody>
+    </CCard>
+  )
+}
+
+export default Vouchers
