a88ef07b437d59d671ecf19ec96499a4a56fd86e
Dương Đức Khải <greenjohnny135@gmail.com>
2024-02-14 07:05:05 +0700

    finish voucher viewing page

--- Frontend/AdminServer/src/views/voucher/Vouchers.js
+++ Frontend/AdminServer/src/views/voucher/Vouchers.js
@@ -12,6 +12,12 @@ import {
   CInputGroupText,
   CLink,
   CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
   CWidgetStatsB,
   CWidgetStatsC,
   CWidgetStatsE,
@@ -115,6 +121,129 @@ const Vouchers = () => {
             </CButton>
           </CCol>
         </CRow>
+
+        <CTable>
+          <CTableHead>
+            <CTableRow>
+              <CTableHeaderCell scope="col">#</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Value</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Description</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Applied Products</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Applied Shops</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Created At</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Expired At</CTableHeaderCell>
+              <CTableHeaderCell scope="col">Status</CTableHeaderCell>
+              <CTableHeaderCell scope="col">No. Handouts</CTableHeaderCell>
+              <CTableHeaderCell scope="col">No. Available</CTableHeaderCell>
+            </CTableRow>
+          </CTableHead>
+          <CTableBody>
+            <CTableRow>
+              <CTableHeaderCell scope="row">1</CTableHeaderCell>
+              <CTableDataCell>BF E 24</CTableDataCell>
+              <CTableDataCell>50000</CTableDataCell>
+              <CTableDataCell>
+                Voucher for electronics products, valid during 2023 blackfriday week.
+              </CTableDataCell>
+              <CTableDataCell>Electronics</CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>16/11/2023</CTableDataCell>
+              <CTableDataCell>23/11/2023</CTableDataCell>
+              <CTableDataCell>Expired</CTableDataCell>
+              <CTableDataCell>2000</CTableDataCell>
+              <CTableDataCell>0</CTableDataCell>
+            </CTableRow>
+
+            <CTableRow>
+              <CTableHeaderCell scope="row">2</CTableHeaderCell>
+              <CTableDataCell>NY FUR 24</CTableDataCell>
+              <CTableDataCell>100000</CTableDataCell>
+              <CTableDataCell>
+                Voucher for Furniture products, valid from 1/1/2024 to 10/1/2024.
+              </CTableDataCell>
+              <CTableDataCell>Furniture</CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>1/1/2024</CTableDataCell>
+              <CTableDataCell>10/1/2024</CTableDataCell>
+              <CTableDataCell>Expired</CTableDataCell>
+              <CTableDataCell>3000</CTableDataCell>
+              <CTableDataCell>0</CTableDataCell>
+            </CTableRow>
+
+            <CTableRow>
+              <CTableHeaderCell scope="row">3</CTableHeaderCell>
+              <CTableDataCell>EMILY V13</CTableDataCell>
+              <CTableDataCell>20000</CTableDataCell>
+              <CTableDataCell>
+                Voucher for Emily store, valid from 10/2/2024 to 14/2/2024.
+              </CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>Emily-2411</CTableDataCell>
+              <CTableDataCell>10/2/2024</CTableDataCell>
+              <CTableDataCell>14/2/2024</CTableDataCell>
+              <CTableDataCell>Valid</CTableDataCell>
+              <CTableDataCell>100</CTableDataCell>
+              <CTableDataCell>3</CTableDataCell>
+            </CTableRow>
+
+            <CTableRow>
+              <CTableHeaderCell scope="row">4</CTableHeaderCell>
+              <CTableDataCell>DUMMY V1</CTableDataCell>
+              <CTableDataCell>100000</CTableDataCell>
+              <CTableDataCell>This is a dummy voucher.</CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>1/1/2024</CTableDataCell>
+              <CTableDataCell>31/12/2024</CTableDataCell>
+              <CTableDataCell>Valid</CTableDataCell>
+              <CTableDataCell>1000</CTableDataCell>
+              <CTableDataCell>578</CTableDataCell>
+            </CTableRow>
+
+            <CTableRow>
+              <CTableHeaderCell scope="row">5</CTableHeaderCell>
+              <CTableDataCell>DUMMY V1</CTableDataCell>
+              <CTableDataCell>100000</CTableDataCell>
+              <CTableDataCell>This is a dummy voucher.</CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>1/1/2024</CTableDataCell>
+              <CTableDataCell>31/12/2024</CTableDataCell>
+              <CTableDataCell>Valid</CTableDataCell>
+              <CTableDataCell>1000</CTableDataCell>
+              <CTableDataCell>578</CTableDataCell>
+            </CTableRow>
+
+            <CTableRow>
+              <CTableHeaderCell scope="row">6</CTableHeaderCell>
+              <CTableDataCell>DUMMY V1</CTableDataCell>
+              <CTableDataCell>100000</CTableDataCell>
+              <CTableDataCell>This is a dummy voucher.</CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>1/1/2024</CTableDataCell>
+              <CTableDataCell>31/12/2024</CTableDataCell>
+              <CTableDataCell>Valid</CTableDataCell>
+              <CTableDataCell>1000</CTableDataCell>
+              <CTableDataCell>578</CTableDataCell>
+            </CTableRow>
+
+            <CTableRow>
+              <CTableHeaderCell scope="row">7</CTableHeaderCell>
+              <CTableDataCell>DUMMY V1</CTableDataCell>
+              <CTableDataCell>100000</CTableDataCell>
+              <CTableDataCell>This is a dummy voucher.</CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>All</CTableDataCell>
+              <CTableDataCell>1/1/2024</CTableDataCell>
+              <CTableDataCell>31/12/2024</CTableDataCell>
+              <CTableDataCell>Valid</CTableDataCell>
+              <CTableDataCell>1000</CTableDataCell>
+              <CTableDataCell>578</CTableDataCell>
+            </CTableRow>
+          </CTableBody>
+        </CTable>
       </CCardBody>
     </CCard>
   )
