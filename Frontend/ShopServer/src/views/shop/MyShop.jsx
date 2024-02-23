c3f85a7c7c70a98a38f77e248c784d2c01e8b7fb
DeItSekiro <tranmanhtien35@gmail.com>
2024-02-23 16:42:46 +0700

    Update Shop detail page + add react bootstrap package 23/2
    use npm install react-bootstrap bootstrap
    to install react bootstrap

--- Frontend/ShopServer/src/views/shop/MyShop.jsx
+++ Frontend/ShopServer/src/views/shop/MyShop.jsx
@@ -1,12 +1,18 @@
 import { lazy } from "react";
 const Search = lazy(() => import("../../components/Search"));
 const ShopInfor  = lazy(() => import("../../components/shop/ShopInfor"));
-
+const ShopProduct  = lazy(() => import("../../components/shop/ShopProduct"));
 
 const MyShopView = () => {
     return(
         <div>
-        <ShopInfor />
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <ShopInfor /> </div>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <ShopProduct />
+        <ShopProduct />
+        <ShopProduct />
+      </div>
         </div>
     )
 };
